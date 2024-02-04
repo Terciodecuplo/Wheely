476b3548cd0936a8815747c5ead32931a00f5d14
Jose Murcia <112321922+Terciodecuplo@users.noreply.github.com>
2024-02-04 18:29:53 +0100

    RecyclerView Post Adapter First Part
    - Added and completed the mandatory methods to work with the recyclerview.
    - Added new pics as example to populate the list.
    - REMEMBER TO CHANGE THE DATA SOURCE.
    - We will need a callback interface in the future.

--- app/src/main/java/com/jmblfma/wheely/adapter/PostsAdapter.kt
+++ app/src/main/java/com/jmblfma/wheely/adapter/PostsAdapter.kt
@@ -1,26 +1,52 @@
 package com.jmblfma.wheely.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.jmblfma.wheely.R
+import com.jmblfma.wheely.model.Post
 
-class PostsAdapter: RecyclerView.Adapter<PostsAdapter.MyHolder>(){
+class PostsAdapter(val postList: ArrayList<Post>, var context: Context) :
+    RecyclerView.Adapter<PostsAdapter.MyHolder>() {
 
-    class MyHolder(item: View): ViewHolder(item){
+    class MyHolder(item: View) : ViewHolder(item) {
+        var userProfileImage: ImageView
+        var userName: TextView
+        var trackInfo: TextView
+        var trackTitle: TextView
+        var trackPreview: ImageView
 
+        init {
+            userProfileImage = item.findViewById(R.id.userPost_image)
+            userName = item.findViewById(R.id.userName_text)
+            trackInfo = item.findViewById(R.id.postInfo_text)
+            trackTitle = item.findViewById(R.id.trackName_text)
+            trackPreview = item.findViewById(R.id.trackPreview_image)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
-        TODO("Not yet implemented")
+        val view: View
+        view = LayoutInflater.from(context).inflate(R.layout.itempost_recycler, parent, false)
+        return MyHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return postList.size
     }
 
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
-        TODO("Not yet implemented")
+        var post = postList[position]
+        holder.userProfileImage.setImageResource(R.drawable.user_default_pic)
+        holder.userName.text = post.userName
+        holder.trackInfo.text = post.postDate.toString()
+        holder.trackTitle.text = post.trackDescription
+        holder.trackPreview.setImageResource(R.drawable.route_example)
     }
 
 
