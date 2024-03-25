522be6aefb38ad6359607b07b318772d1c0b226f
Jose Murcia <112321922+Terciodecuplo@users.noreply.github.com>
2024-03-25 19:53:54 +0100

    Track History List for Profile
    - Creation of the Adapter and recycler view.

--- app/src/main/java/com/jmblfma/wheely/ProfilePageActivity.kt
+++ app/src/main/java/com/jmblfma/wheely/ProfilePageActivity.kt
@@ -1,17 +1,26 @@
 package com.jmblfma.wheely
 
 import android.os.Bundle
+import com.jmblfma.wheely.adapter.TrackHistoryAdapter
 import com.jmblfma.wheely.databinding.UserProfileMainBinding
+import com.jmblfma.wheely.model.Post
 import com.jmblfma.wheely.utils.NavigationMenuActivity
+import java.util.ArrayList
 
 class ProfilePageActivity : NavigationMenuActivity() {
     private lateinit var binding: UserProfileMainBinding
+    private lateinit var trackHistoryAdapter: TrackHistoryAdapter
+    private lateinit var trackHistoryList: ArrayList<Post>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = UserProfileMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupBottomNavigation()
+
+        trackHistoryList = ArrayList()
+
     }
 
     override fun getBottomNavigationMenuItemId(): Int {
