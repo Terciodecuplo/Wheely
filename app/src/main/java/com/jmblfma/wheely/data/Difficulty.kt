e23ce84d83ce3c9fcdc2132e7e3f6c75124c3dad
Luis F. Martínez <lfmrad@gmail.com>
2024-05-19 10:58:30 +0200

    TrackViewer UI Save Screen Completed

--- app/src/main/java/com/jmblfma/wheely/data/Difficulty.kt
+++ app/src/main/java/com/jmblfma/wheely/data/Difficulty.kt
@@ -1,18 +1,21 @@
 package com.jmblfma.wheely.data
+import androidx.core.content.ContextCompat.getString
 import androidx.room.TypeConverter
+import com.jmblfma.wheely.MyApp
+import com.jmblfma.wheely.R
 
 enum class Difficulty(val value: Int) {
     UNKNOWN(0){
-        override fun toString() = "Unknown"
+        override fun toString() = getString(MyApp.applicationContext(), R.string.difficulty_none)
     },
     EASY(1){
-        override fun toString() = "Easy"
+        override fun toString() = getString(MyApp.applicationContext(), R.string.difficulty_easy)
     },
     MEDIUM(2){
-        override fun toString() = "Medium"
+        override fun toString() = getString(MyApp.applicationContext(), R.string.difficulty_medium)
     },
     HARD(3){
-        override fun toString() = "Hard"
+        override fun toString() = getString(MyApp.applicationContext(), R.string.difficulty_hard)
     };
 
     companion object {
